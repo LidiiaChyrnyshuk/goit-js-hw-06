4fa46f9cd16d1d3ef5850a19c005a3679e2f60f0
Lidiia Chyrnyshuk <l.mandych@gmail.com>
2023-02-27 15:20:52 +0200

    add final task-10

--- js/task-10.js
+++ js/task-10.js
@@ -14,20 +14,19 @@ buttonDestroyEl.addEventListener("click", destroyBoxes);
 
 function destroyBoxes() {
   boxEl.innerHTML = "";
-  
+  inputEl.value = '';
 }
 
 function createBoxes() {
   const amount = inputEl.value;
-	// let currentColor = getRandomHexColor();
 	let height = 30;
   let width = 30;
   const collectionEl = [];
 	for ( let i = 0; i < amount; i += 1) {
     let newDivEl = document.createElement('div');
 		newDivEl.style.backgroundColor = getRandomHexColor();
-		newDivEl.style.height = `${height + 10 * i} px`;
-    newDivEl.style.width = `${width + 10 * i} px`;
+		newDivEl.style.height = `${height + 10 * i}px`;
+    newDivEl.style.width = `${width + 10 * i}px`;
     console.log(newDivEl);
     collectionEl.push(newDivEl);
   }
